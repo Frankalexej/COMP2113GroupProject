cff3a19c7ec122715c78259e2d04c0cce028de98
Kelly-0628 <61001732+Kelly-0628@users.noreply.github.com>
2020-05-09 21:28:21 +0800

    upload point.h and point.cpp

---
+++ Point.cpp
@@ -0,0 +1,78 @@
+﻿#include "Point.h"
+#include "Map.h"	// avoid self-inclusion, in co-operation with class Map in Point.h
+#include <string>
+using namespace std;
+
+Point::Point(int x, int y, OccupierPasser* op) : x(x), y(y) {
+	this->info = new Pass();
+	this->isOcc = true;
+	this->info->op = op;
+}
+
+Point::Point(int x, int y, TerrainPasser* tp) : x(x), y(y) {
+	this->info = new Pass();
+	this->isOcc = false;
+	this->info->tp = tp;
+}
+
+Point::~Point() {
+	// delete the Passers
+	if (this->isOccupier()) delete this->info->op;
+	else delete this->info->tp;
+	delete this->info;
+}
+
+bool Point::isOccupier() {
+	return this->isOcc;
+}
+
+string Point::getType() {
+	return this->isOccupier() ? this->info->op->getType() : this->info->tp->getType();
+}
+
+bool Point::getSide() {
+	return this->info->op->getSide();
+}
+
+int Point::getAttack() {
+	return this->isOccupier() ? this->info->op->getAttack() : 0;
+}
+
+int Point::getLife() {
+	return this->isOccupier() ? this->info->op->getLife() : 1000;	// very large is impossible
+}
+
+int Point::getPortability() {
+	return this->isOccupier() ? this->info->op->getPortability() : 0;
+}
+
+int Point::getWas() {
+	if (this->isOccupier()) return this->info->op->getWas();
+	else return this->info->tp->getWas();
+}
+
+void Point::setWas(int was) {
+	if (this->isOccupier()) this->info->op->setWas(was);
+	return;
+}
+
+void Point::setLife(int life)
+{
+	if (!this->isOccupier()) return;
+	this->info->op->setLife(life);
+}
+
+string Point::getPrint() {
+	//if (this->isOccupier()) {
+	//	string x = this->info->op->getRepres();
+	//	return this->getSide() ? KRED + x + RST : KYEL + x + RST;
+	//}
+	//else return this->info->tp->getRepres();
+
+	return this->isOccupier() ? ((this->getSide() ? KRED : KYEL) + this->info->op->getRepres() + RST) : this->info->tp->getRepres();
+}
+
+bool Point::canEnter(Point* p) {
+	if (this->isOccupier()) return (this->getSide() != p->getSide());	// cannot enter if of same side
+	else return this->getWas() > 0;	// cannot enter if is Terrain but is hill or sea
+}
---
+++ Point.h
@@ -0,0 +1,51 @@
+﻿#include <string>
+#include "OccupierPasser.h"
+#include "TerrainPasser.h"
+using namespace std;
+#pragma once
+
+class Map;	// this is to overcome the problem of self-inclusion
+
+class Point
+{
+	// this represents one point on the map
+	// we didn't choose to organise different types of Points as a family of classes
+	// because C++ doesn't support polymorphism well
+	// problems may occur when objects of differnt sizes are put into the same array
+private:
+	// common variables
+	bool isOcc;	// specifies whether it is an Occupier or not (false if is Terrain)
+
+	// union can save some memory, because a Point is either an Occupier or a Terrain
+	union Pass
+	{
+		OccupierPasser* op;	// occupier only
+		TerrainPasser* tp;	// terrain only
+	};
+	Pass* info;
+	Map* map;
+public:
+	int x;	// horizontal position
+	int y;	// vertical position
+
+	Point(int x, int y, OccupierPasser* op);	// new an occupier
+	Point(int x, int y, TerrainPasser* tp);		// new a terrain
+	~Point(); // destructer, to handle deletion of Passers
+
+	bool isOccupier();	// returns isOcc
+
+	string getType();	// returns type
+	string getPrint();	// returned string includes side colour (Red for A, Yellow for B)
+	bool getSide();	// if a point Terrain calls it, it would cause problem
+	// simple getters
+	int getAttack();
+	int getLife();
+	int getPortability();
+	int getWas();	// pass was if isOccupier, else pass productivity
+
+	void setWas(int was);	// sets the was property of Occupiers
+	void setLife(int life); // sets the life property of Occupiers
+
+	bool canEnter(Point* p);	// return whether p can enter this Point (this)
+};
+
