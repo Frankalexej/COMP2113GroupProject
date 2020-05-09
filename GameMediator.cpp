f9c118343263228382ed258ad40688150301b4f8
Frankalexej <47711954+Frankalexej@users.noreply.github.com>
2020-05-09 21:34:03 +0800

    Added GameMediator header and source file
    GameMediator is a class for keeping cross-round, non-map infos (turn counter & money counter)

---
+++ GameMediator.cpp
@@ -0,0 +1,24 @@
+﻿#include "GameMediator.h"
+
+GameMediator::GameMediator() :isTurn(true), AMoney(0), BMoney(0) {}
+GameMediator::GameMediator(bool isTurn, int AMoney, int BMoney) :isTurn(isTurn), AMoney(AMoney), BMoney(BMoney) {}
+int GameMediator::getMoney()
+{
+	return this->isTurn ? this->AMoney : this->BMoney;
+}
+void GameMediator::setMoney(int money)
+{
+	(this->isTurn ? this->AMoney : this->BMoney) = money;
+}
+
+void GameMediator::accMoney(int AMoneyAcc, int BMoneyAcc)
+{
+	this->AMoney += AMoneyAcc;
+	this->BMoney += BMoneyAcc;
+}
+
+bool GameMediator::nextTurn()
+{
+	this->isTurn = !this->isTurn;
+	return this->isTurn;
+}
---
+++ GameMediator.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+class GameMediator
+{
+	// GameMediator counts turns and money, it doesn't have access to other objects, therefore, it must accept infos passed in so as for update
+	// there will be only one GameMediator in each execution
+public:
+	int AMoney;
+	int BMoney;
+	bool isTurn;	// note: true means turn for A, false for B
+
+	GameMediator();	// defaut is A's turn, both no money
+	GameMediator(bool isTurn, int AMoney, int BMoney);	// used for recovering from savings
+
+	int getMoney();	// returns AMoney or BMoney, based on the turn
+	void setMoney(int money);	// can update money, also based on turn
+
+	void accMoney(int AMoneyAcc, int BMoneyAcc);	// accumulate money, used after each round
+	bool nextTurn();	// next turn, just do '!'
+};
