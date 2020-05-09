27e33e528b879ea1833f2237cd8ddbcf0b94e25e
Frankalexej <47711954+Frankalexej@users.noreply.github.com>
2020-05-09 21:28:30 +0800

    Added SaveReadWriter header file and source file
    This class contain two methods: converting line to Point object and vice versa.

---
+++ SaveReadWriter.cpp
@@ -0,0 +1,29 @@
+﻿#include "SaveReadWriter.h"
+// model: "x y type side attack life portability repres"
+
+SaveReadWriter::SaveReadWriter(Initialiser* init) :init(init) {}
+
+Point* SaveReadWriter::occupierRead(string line) {
+	this->ins.str(line);
+	int x, y, attack, life, portability;
+	string type, repres;
+	bool side;
+	this->ins >> x >> y >> type >> side >> attack >> life >> portability;
+	this->ins.clear();
+	return new Point(x, y, new OccupierPasser(side, type, attack, life, portability, this->init->getRepres(type)));
+
+	//this->ss.str(line);
+	//int x, y, attack, life, portability;
+	//string type, repres;
+	//bool side;
+	//this->ss >> x >> y >> type >> side >> attack >> life >> portability;
+	//this->ss.str("");
+	//return new Point(x, y, new OccupierPasser(side, type, attack, life, portability, this->init->getRepres(type)));
+}
+
+string SaveReadWriter::occupierWrite(Point* p) {
+	this->os.str("");
+	this->os << p->x << " " << p->y << " " << p->getType() << " " << p->getSide() << " " << p->getAttack() << " " << p->getLife() << " " << p->getPortability() << "\n";
+	string toWrite = this->os.str();
+	return toWrite;
+}
---
+++ SaveReadWriter.h
@@ -0,0 +1,22 @@
+﻿#include "Point.h"
+#include "Initialiser.h"
+#include <sstream>
+#pragma once
+class SaveReadWriter
+{
+	// this class is used for transformation between saving data and Occupier point
+	// note that this is for Occupiers only
+	// terrain is much simpler but require more complicated positioning techniques
+	// because terrain info is listed as lines of space-separated ints, whose position needs inference from the whole map
+private:
+	istringstream ins;
+	ostringstream os;
+	//stringstream ss;
+	Initialiser* init;	// just used to pass repres
+public:
+	// model: "x y type side attack life portability"
+	SaveReadWriter(Initialiser* init);
+	Point* occupierRead(string line);	// raed a line in saving and reconstruct a point
+	string occupierWrite(Point* p);	// transform point to string
+};
+
